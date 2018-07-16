9bbbcda74665d28113d61d631b9c08bec245345d
Jonathan Moallem <jdmoallem@gmail.com>
2018-07-16 13:52:03 +1000

    Initialised application files

---
+++ src/frost-crystal.cr
@@ -0,0 +1,6 @@
+require "./frost-crystal/*"
+
+# TODO: Write documentation for `Frost::Crystal`
+module Frost::Crystal
+  # TODO: Put your code here
+end
---
+++ src/frost-crystal/version.cr
@@ -0,0 +1,3 @@
+module Frost::Crystal
+  VERSION = "0.1.0"
+end
