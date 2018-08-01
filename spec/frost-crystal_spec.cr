dd23ffcb87c3f8b8a120dc9cd24599dd54631f6e
Jonathan Moallem <jdmoallem@gmail.com>
2018-08-01 13:44:24 +1000

    Added Mass Spec for testing

--- spec/frost-crystal_spec.cr
+++ spec/frost-crystal_spec.cr
@@ -1,9 +1,16 @@
 require "./spec_helper"
 
-describe Frost::App do
-  # TODO: Write tests
-
+describe Frost::Main do
   it "works" do
     false.should eq(true)
+  end
+
+  it "returns the path in json" do
+    get("/api/v1/user")
+
+    status_code.should eq(200)
+    headers.should contain({"Content-Type", ["application/json"]})
+    # body.should eq(%({"path":"/nas/beru/uhn'adarr"}))
+    # json_body.should eq({"path" => "/api/v1/user"})
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,9 @@
+ENV["ENV"] = "test"
+
+# Require and include all testing libraries
 require "spec"
+require "mass_spec"
+include MassSpec::GlobalDSL
+
+# Start the server
 require "../src/frost-crystal"
