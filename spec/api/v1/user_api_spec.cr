d82e2fe8ff930e997ed6c86b8ea0f8bfc83c56fe
Jonathan Moallem <jdmoallem@gmail.com>
2018-08-02 16:05:05 +1000

    Added user show route spec

--- spec/api/v1/user_api_spec.cr
+++ spec/api/v1/user_api_spec.cr
@@ -3,15 +3,20 @@ require "../../spec_helper"
 describe V1::UserApi do
   describe "index route" do
     get("/api/v1/user")
-    it "returns a success" do
-      status_code.should eq(200)
-      # headers.should contain({"Content-Type", ["application/json"]})
-      # body.should eq(%({"path":"/nas/beru/uhn'adarr"}))
-      # json_body.should eq({"path" => "/api/v1/user"})
-    end
 
-    it "returns all users" do
+    it "successfully returns all users" do
+      status_code.should eq(200)
       body.should eq(User.all.to_json)
+    end
+  end
+  describe "show route" do
+    if user = User.first
+      get("/api/v1/user/#{user.id}")
+
+      it "successfully returns the user" do
+        status_code.should eq(200)
+        body.should eq(user.to_json)
+      end
     end
   end
 end
