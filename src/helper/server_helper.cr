3255b7d3b5614cc89a2515bdca4ad3e20edadab9
Jonathan Moallem <jdmoallem@gmail.com>
2018-10-23 23:51:23 -0400

    Simplified server start method

--- src/helper/server_helper.cr
+++ src/helper/server_helper.cr
@@ -1,10 +1,6 @@
 module Frost::ServerHelper
   def start_server(on port : Int32)
-    start_message(port)
-    FrostApp.run(port)
-  end
-
-  private def start_message(port : Int32)
     puts "Running app version #{VERSION}"
+    FrostApp.run(port)
   end
 end
